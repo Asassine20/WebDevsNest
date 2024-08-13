8b21fdbe6fbc6c360a0d052551be5960f636ed1c
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2024-08-12 22:16:56 -0500

    Updated search api to make suggested items more accurate

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -8,10 +8,28 @@ export default async (req, res) => {
   }
 
   const connection = await createConnection();
+
   const [rows] = await connection.execute(
-    'SELECT Id, Title, Category, Slug FROM Post WHERE Title LIKE ? OR Content LIKE ?',
-    [`%${query}%`, `%${query}%`]
+    `
+    SELECT Id, Title, Category, Slug,
+           CASE
+             WHEN Title LIKE ? THEN 1
+             WHEN Title LIKE ? THEN 2
+             ELSE 3
+           END AS \`Rank\`
+    FROM Post
+    WHERE Title LIKE ? OR Content LIKE ?
+    ORDER BY \`Rank\`, LOCATE(?, Title), Title ASC
+    `,
+    [
+      `${query}%`,         // Exact match at the start of the Title
+      `%${query}%`,        // Match anywhere in the Title
+      `%${query}%`,        // For WHERE clause Title
+      `%${query}%`,        // For WHERE clause Content
+      query                // For LOCATE function
+    ]
   );
+
   await connection.end();
 
   return res.status(200).json(rows);
